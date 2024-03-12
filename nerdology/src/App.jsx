57db2195cebf894f012e8f4baaaa6fb5f7c04f95
AnBC22 <104400577+AnBC22@users.noreply.github.com>
2024-03-12 14:18:56 -0600

    Commit initial files
    Deleted Vite's prebuilt project
    Added a new component called 'StartPage' just for testing purposes.

---
+++ nerdology/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import StartPage from './pages/StartPage'
+
+function App() {
+
+  return (
+    <>
+      <StartPage />
+    </>
+  )
+}
+
+export default App
---
+++ nerdology/src/pages/StartPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// import './App.css'
+
+export default function StartPage() {
+
+  return (
+    <>
+        <h2>This is the start page</h2>
+    </>
+  )
+}
