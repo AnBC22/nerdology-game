3b35d57408ef2b1e699883a9b4341370a1bd892c
AnBC22 <104400577+AnBC22@users.noreply.github.com>
2024-03-18 15:58:51 -0600

    Added react router

--- nerdology/src/App.jsx
+++ nerdology/src/App.jsx
@@ -1,78 +1,42 @@
 import React from 'react'
+/* React router */
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+/* ------------- */
 import './App.css'
 import StartPage from './pages/StartPage'
 import Quiz from './pages/Quiz'
 import WaitingTime from './pages/WaitingTime'
-const triviaURL = 'https://opentdb.com/api.php?amount=3&difficulty=easy'
 
 function App() {
 
-  let pageLoaded = true
-
-  const [ startGame, setStartGame ] = React.useState(false)
-  const [ triviaData, setTriviaData ] = React.useState(null)
-  const [ startNewGame, setStartNewGame ] = React.useState(false)
-  const [ waitingTime, setWaitingTime ] = React.useState(false)
-
-  function handleStartGame() {
-    console.log('Game started')
-    setStartGame(prev => !prev)
-  }
-
-  function handleStartNewGame() {
-    setWaitingTime(true)
-    setTimeout(() => {
-      setStartNewGame(prev => !prev)
-    }, 4000)
-
-    setTimeout(() => {
-      handleWaitingTime()
-    }, 5100)
-  }
-
-  function handleWaitingTime() {
-    setWaitingTime(false)
-  }
-
-  React.useEffect(() => {
-    async function getQuestionsData() {
-
-      try {
-        const response = await fetch(triviaURL)
-        if(!response.ok) {
-          throw new Error('Network response failed')
-        }
-        const data = await response.json()
-        setTriviaData(data)
-      }
-
-      catch(error) {
-        console.error('Error fetching trivia data:', error.message)
-        alert("There was an error fetching data for the quiz")
-      }
-
-    }
-
-    if(pageLoaded) {
-      getQuestionsData()
-    }
-
-    pageLoaded = false
-
-  }, [startNewGame])
-
-
   return (
     <>
-      {
-          waitingTime ? 
-          <WaitingTime/>: 
-          startGame ? 
-          <Quiz triviaData={triviaData} handleStartNewGame={handleStartNewGame}/> : 
-          <StartPage handleStartGame={handleStartGame} />
-      }
+
+    <BrowserRouter>
+      <header>
+        <h3>Some information that will be available to all pages (like links)</h3>
+      </header>
+      <Routes>
+        <Route path="/" element={<StartPage />}/>
+        <Route path="/quiz" element={<Quiz />}/>
+        <Route path="/waitingTime" element={<WaitingTime/>}/>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
 
 export default App
+
+/*
+
+
+{
+    waitingTime ? 
+    <WaitingTime/>: 
+    startGame ? 
+     : 
+}
+
+*/
--- nerdology/src/pages/Quiz.jsx
+++ nerdology/src/pages/Quiz.jsx
@@ -1,12 +1,68 @@
 import React from 'react'
 import Button from '../components/Button'
-import GameOver from './GameOver'
 
-// import './App.css'
+export default function Quiz() {
 
-export default function Quiz({ triviaData, handleStartNewGame }) {
+    const triviaURL = 'https://opentdb.com/api.php?amount=3&difficulty=easy'
 
-    const [ checkAnswers, setCheckAnswers ] = React.useState(false)
+    // const [ startGame, setStartGame ] = React.useState(false)
+    // const [ startNewGame, setStartNewGame ] = React.useState(false)
+    // const [ waitingTime, setWaitingTime ] = React.useState(false)
+    // const [ checkAnswers, setCheckAnswers ] = React.useState(false)
+
+    const [ triviaData, setTriviaData ] = React.useState(null)
+    
+    let pageLoaded = true
+
+/*
+    function handleStartGame() {
+        console.log('Game started')
+        setStartGame(prev => !prev)
+      }
+    
+      function handleStartNewGame() {
+        setWaitingTime(true)
+        setTimeout(() => {
+          setStartNewGame(prev => !prev)
+        }, 4000)
+    
+        setTimeout(() => {
+          handleWaitingTime()
+        }, 5100)
+      }
+    
+      function handleWaitingTime() {
+        setWaitingTime(false)
+      }
+*/
+
+
+    React.useEffect(() => {
+        async function getQuestionsData() {
+    
+          try {
+            const response = await fetch(triviaURL)
+            if(!response.ok) {
+              throw new Error('Network response failed')
+            }
+            const data = await response.json()
+            setTriviaData(data)
+          }
+    
+          catch(error) {
+            console.error('Error fetching trivia data:', error.message)
+            alert("There was an error fetching data for the quiz")
+          }
+    
+        }
+        if(pageLoaded) {
+          getQuestionsData()
+        }
+    
+        pageLoaded = false
+    
+      }, [])
+
 
     function insertRandom(array, newElement) {
         const randomIndex = Math.floor(Math.random() * array.length)
@@ -15,82 +71,95 @@ export default function Quiz({ triviaData, handleStartNewGame }) {
         return newArray
     }
 
+    /*
+
     function handleShowAnswers() {
         console.log("Show answers")
         setCheckAnswers(true)
     }
 
+    */
 
-  const [minutes, setMinutes] = React.useState(1);
-  const [seconds, setSeconds] = React.useState(0);
+    const [minutes, setMinutes] = React.useState(1);
+    const [seconds, setSeconds] = React.useState(0);
 
-  React.useEffect(() => {
-    const timer = setInterval(() => {
-      setSeconds(prevSeconds => {
-        if (prevSeconds === 0) {
-          // If seconds reach 0, decrease minutes and reset seconds
-          if (minutes === 0) {
-            clearInterval(timer); // Stop the timer when it reaches 0:00
-            return 0;
-          }
-          setMinutes(prevMinutes => prevMinutes - 1);
-          return 59;
-        }
-        return prevSeconds - 1;
-      });
-    }, 1000);
-    // Cleanup function to clear interval when component unmounts
-    return () => clearInterval(timer);
-  }, [minutes]); // Re-run effect only when minutes change
-
-
+    React.useEffect(() => {
+        const timer = setInterval(() => {
+        setSeconds(prevSeconds => {
+            if (prevSeconds === 0) {
+            // If seconds reach 0, decrease minutes and reset seconds
+            if (minutes === 0) {
+                clearInterval(timer); // Stop the timer when it reaches 0:00
+                return 0;
+            }
+            setMinutes(prevMinutes => prevMinutes - 1);
+            return 59;
+            }
+            return prevSeconds - 1;
+        });
+        }, 1000);
+        // Cleanup function to clear interval when component unmounts
+        return () => clearInterval(timer);
+    }, [minutes]); // Re-run effect only when minutes change
 
 
-    const triviaDataHtml = triviaData.results.map((questionObj, index) => {
 
-        const incorrectAnswers = questionObj.incorrect_answers
+
+    const [ xx, setXx ] = React.useState(null)
+
+    React.useEffect(() => {
+        let triviaDataHtml = null
+
+        if(triviaData) {
+            triviaDataHtml = triviaData.results.map((questionObj, index) => {
+
+                const incorrectAnswers = questionObj.incorrect_answers
+                
+                const allAnswers = insertRandom(incorrectAnswers, questionObj.correct_answer)
         
-        const allAnswers = insertRandom(incorrectAnswers, questionObj.correct_answer)
+                const allAnswersHtml = allAnswers.map(answer => {
+                    return (
+                        <p>{answer}</p>
+                    )
+                })
+        
+                return ( //End of triviaDataHtml
+                    <div key={index}>
+                        <h2>
+                            {questionObj.question}
+                        </h2>
+                        {allAnswersHtml}
+                    </div>
+                )
+            })
+        }
 
-        const allAnswersHtml = allAnswers.map(answer => {
-            return (
-                <p>{answer}</p>
-            )
-        })
+        setXx(triviaDataHtml)
+    }, [triviaData])
 
-        return ( //End of triviaDataHtml
-            <div key={index}>
-                <h2>
-                    {questionObj.question}
-                </h2>
-                {allAnswersHtml}
-            </div>
-        )
-    })
 
     return ( //End of Quiz function
         <>
             <h2>This is the Quiz</h2>
             <h1>{`${minutes}:${seconds < 10 ? '0' : ''}${seconds}`}</h1>
-            {triviaDataHtml}
-            {
-                checkAnswers ? 
-                <Button 
-                    buttonAction={handleStartNewGame} 
-                    buttonText={'Play again'}
-                    buttonSize={'small'}
-                >
-                    Play again
-                </Button> :
+            {xx}
+            
+            <Button 
+                buttonAction={null} 
+                buttonText={'Play again'}
+                buttonSize={'small'}
+            >
+                Play again
+            </Button> 
 
-                <Button 
-                    buttonAction={handleShowAnswers} 
-                    buttonText={'Check Answers'}
-                    buttonSize={'small'}
-                >
-                    Check answers
-                </Button>
-            }
+            <Button 
+                buttonAction={null} 
+                buttonText={'Check Answers'}
+                buttonSize={'small'}
+            >
+                Check answers
+            </Button>
+            
         </>
     )
 }
