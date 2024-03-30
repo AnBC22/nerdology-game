7d43e053b5fd3b517407560c1ae57aeb04447f47
AnBC22 <104400577+AnBC22@users.noreply.github.com>
2024-03-30 13:03:42 -0600

    Refactored

--- nerdology/src/pages/Quiz.jsx
+++ nerdology/src/pages/Quiz.jsx
@@ -1,104 +1,18 @@
 import React from 'react'
 import Button from '../components/Button/Button'
 import { Link } from "react-router-dom"
-import { set } from 'lodash'
 import AnswerButton from '../components/AnswerButton/AnswerButton'
 
 export default function Quiz({ triviaData, handleNewDataRequest }) {
 
     const [ checkAnswers, setCheckAnswers ] = React.useState(false)
-    const [ numberOfCorrectAnswers, setNumberOfCorrectAnswers ] = React.useState(0)
-
-
-    //Remember, the idea in this branch is that the user is only able to select one answer per question.
-
-    function handleCorrectAnswerSelected() {
-        setNumberOfCorrectAnswers(prevAnswers => prevAnswers + 1)
-    }
-
-
-    function handleClickedAnswer(responseValidity) {
-        if(responseValidity === 'correct') {
-            console.log('Congratulations')
-        } else {
-            console.log('Wrong answer')
-        }
-    }
-
-    function getCombinedAnswersArray(array, newElement) {
-        const incorrectAnswers = array.map((incorrectAnswer, incorrectAnswerIndex) => {
-            return (
-                <AnswerButton
-                    key={incorrectAnswerIndex}
-                    clicked={() => handleClickedAnswer('incorrect')}
-                >
-                    {incorrectAnswer}
-                </AnswerButton>
-            )
-        })
-
-        const correctAnswer = (
-            <AnswerButton
-                key={3}
-                clicked={() => handleClickedAnswer('correct')}
-            >
-                {newElement}
-            </AnswerButton>
-        )
-        const randomquestionIndex = Math.floor(Math.random() * (incorrectAnswers.length + 1))
-        const randomquestionIndexRef = React.useRef(randomquestionIndex)
-        const combinedAnswersArray = incorrectAnswers.toSpliced(randomquestionIndexRef.current, 0, correctAnswer)
-        return combinedAnswersArray
-    }
-
-
-
-    function handleShowAnswers() {
-        console.log("Show answers")
-        setCheckAnswers(true)
-    }
-
     
-    
-
-  const [minutes, setMinutes] = React.useState(1);
-  const [seconds, setSeconds] = React.useState(0);
-
-  React.useEffect(() => {
-    const timer = setInterval(() => {
-      setSeconds(prevSeconds => {
-        if (prevSeconds === 0) {
-          // If seconds reach 0, decrease minutes and reset seconds
-          if (minutes === 0) {
-            clearInterval(timer); // Stop the timer when it reaches 0:00
-            return 0;
-          }
-          setMinutes(prevMinutes => prevMinutes - 1);
-          return 59;
-        }
-        return prevSeconds - 1;
-      });
-    }, 50);
-    // Cleanup function to clear interval when component unmounts
-    return () => clearInterval(timer);
-  }, [minutes]); // Re-run effect only when minutes change
-
-
     const triviaDataHtml = triviaData.results.map((questionObj, questionIndex) => {
-
         const incorrectAnswers = questionObj.incorrect_answers
-        
+
         const allAnswers = getCombinedAnswersArray(incorrectAnswers, questionObj.correct_answer)
 
-        /*
-        const allAnswersHtml = allAnswers.map(answer => {
-            return (
-                <button className="answer-button">{answer}</button>
-            )
-        })
-        */
-
-        return ( //End of triviaDataHtml
+        return (
             <div key={questionIndex}>
                 <h2>
                     {questionObj.question}
@@ -109,6 +23,70 @@ export default function Quiz({ triviaData, handleNewDataRequest }) {
             </div>
         )
     })
+
+    function getCombinedAnswersArray(array, newElement) {
+        const incorrectAnswers = array.map((incorrectAnswer, incorrectAnswerIndex) => {
+            return (
+                <AnswerButton
+                    key={incorrectAnswerIndex}
+                    clicked={() => handleClickedAnswer(false)}
+                >
+                    {incorrectAnswer}
+                </AnswerButton>
+            )
+        })
+
+        const correctAnswer = (
+            <AnswerButton
+                key={3}
+                clicked={() => handleClickedAnswer(true)}
+            >
+                {newElement}
+            </AnswerButton>
+        )
+        const randomquestionIndex = Math.floor(Math.random() * (incorrectAnswers.length + 1))
+        const randomquestionIndexRef = React.useRef(randomquestionIndex)
+        const combinedAnswersArray = incorrectAnswers.toSpliced(randomquestionIndexRef.current, 0, correctAnswer)
+        return combinedAnswersArray
+    }
+
+    function handleClickedAnswer(isCorrect) {
+        if(isCorrect) {
+            console.log('Congratulations')
+        } else {
+            console.log('Wrong answer')
+        }
+    }
+
+
+    function handleShowAnswers() {
+        console.log("Show answers")
+        setCheckAnswers(true)
+    }
+
+    
+    const [minutes, setMinutes] = React.useState(1);
+    const [seconds, setSeconds] = React.useState(0);
+
+    React.useEffect(() => {
+        const timer = setInterval(() => {
+        setSeconds(prevSeconds => {
+            if (prevSeconds === 0) {
+            // If seconds reach 0, decrease minutes and reset seconds
+            if (minutes === 0) {
+                clearInterval(timer); // Stop the timer when it reaches 0:00
+                return 0;
+            }
+            setMinutes(prevMinutes => prevMinutes - 1);
+            return 59;
+            }
+            return prevSeconds - 1;
+        });
+        }, 50);
+        // Cleanup function to clear interval when component unmounts
+        return () => clearInterval(timer);
+    }, [minutes]); // Re-run effect only when minutes change
+
 
     return ( //End of Quiz function
         <>
